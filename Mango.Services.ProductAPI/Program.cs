68349c71e10cfc106134c9f7661c51a37fa76eff
IvanKrutik <ivan.krutik@gmail.com>
2023-02-19 18:52:27 +0400

    ProductRepository

--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -1,5 +1,8 @@
 
+using AutoMapper;
 using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Mapping;
+using Mango.Services.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +16,12 @@ builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
---
+++ Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.ProductAPI.Models.Dto;
+using System.Collections;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+
+        Task<ProductDto> GetProductById(long productId);
+
+        Task<ProductDto> CreateUpdateProduct(ProductDto product);
+
+        Task<bool> DeleteProduct(long productId);
+    }
+}
