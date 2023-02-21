e6c56e98316451946e37c88ad76a37fcb7247a5b
IvanKrutik <ivan.krutik@gmail.com>
2023-02-21 19:52:33 +0400

    Добавил ProductView & ProductController

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new List<ProductDto>();
+            var responce = await _productService.GetProductsAllAsync<ResponseDto>();
+            if (responce != null && responce.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(responce.Result));
+            }
+
+            return View(list);
+        }
+    }
+}
