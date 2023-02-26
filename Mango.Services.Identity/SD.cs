fc352dcf41c5d9d5a2a436673367738308c4f3e7
IvanKrutik <ivan.krutik@gmail.com>
2023-02-26 14:29:07 +0400

    temp

---
+++ Mango.Services.Identity/SD.cs
@@ -0,0 +1,45 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Mango.Services.Identity
+{
+    public static class SD
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>()
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email()
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>()
+        {
+            new ApiScope("mango", "Mango server"),
+            new ApiScope("read", "Read your data"),
+            new ApiScope("write", "Write your data"),
+            new ApiScope("delete", "Delete your data")
+        };
+
+        public static IEnumerable<Client> Clients => new List<Client>()
+        {
+            new Client()
+            {
+                ClientId = "client",
+                ClientSecrets = {new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = {"read", "write", "profile"}
+            },
+
+            new Client()
+            {
+                ClientId = "mango",
+                ClientSecrets = {new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = {"https://localhost:44344" },
+                AllowedScopes = {"read", "write", "profile"}
+            }
+        };
+    }
+}
