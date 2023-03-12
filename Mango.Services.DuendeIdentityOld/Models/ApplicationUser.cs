8f8d889e99fe5e9f11b7ce9c32dafc396b8d06ec
IvanKrutik <ivan.krutik@gmail.com>
2023-03-12 16:00:06 +0400

    С помощью магии и такой то матери зарустил версию сервера 5.1.1 To be continued

---
+++ Mango.Services.DuendeIdentityOld/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Duende.Services.IdentityNew.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Mango.Services.DuendeIdentityOld/SD.cs
@@ -0,0 +1,53 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace Duende.Services.IdentityNew
+{
+    public static class SD
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+        public static IEnumerable<IdentityResource> IdentityResources => new List<IdentityResource>()
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email()
+        };
+
+        public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope>()
+        {
+            new ApiScope("mango", "Mango server"),
+            new ApiScope("read", "Read your data"),
+            new ApiScope("write", "Write your data"),
+            new ApiScope("delete", "Delete your data")
+        };
+
+        public static IEnumerable<Client> Clients => new List<Client>()
+        {
+            new Client()
+            {
+                ClientId = "client",
+                ClientSecrets = {new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                AllowedScopes = {"read", "write", "profile"}
+            },
+
+            new Client()
+            {
+                ClientId = "mango",
+                ClientSecrets = {new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.Code,
+                RedirectUris = {"https://localhost:44344/signin-oidc" , "https://localhost:7141/signin-oidc", "http://localhost:5269/signin-oidc"},
+                PostLogoutRedirectUris= {"https://localhost:44344/signout-callback-oidc", "https://localhost:7141/signout-callback-oidc", "http://localhost:5269/signout-callback-oidc" },
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Email,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "mango"
+                }
+            }
+        };
+    }
+}
